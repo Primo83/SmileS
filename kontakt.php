ae97f5bcfa50371acd92f0d172dcce52eecf1b50
Primo <wolnikpiotr@gmail.com>
2016-03-22 19:44:52 +0100

    dwukropek

---
+++ kontakt.php
@@ -0,0 +1,101 @@
+<!--==============================content================================-->
+<section id="content">
+	<div class="container_12">
+		<div class="grid_12">
+			<div class="inner-block">
+				<h2 class="padd-1">Bydgoszcz Fordon</h2>
+				<div class="grid_7 marg-left-0">
+
+					<iframe id="map_canvas"
+						src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=pl&amp;geocode=&amp;q=gimnazjum+nr+3+bydgoszcz&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=59.50923,114.169922&amp;t=h&amp;ie=UTF8&amp;hq=gimnazjum+nr+3&amp;hnear=Bydgoszcz,+Kujawsko-Pomorskie,+Polska&amp;cid=2821239990550035673&amp;ll=53.178469,18.174133&amp;spn=0.072017,0.164795&amp;z=12&amp;iwloc=A&amp;output=embed"
+						frameborder="0" style="border: 0"></iframe>
+
+					<!--  <iframe width="480" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=pl&amp;geocode=&amp;q=gimnazjum+nr+3+bydgoszcz&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=59.50923,114.169922&amp;t=h&amp;ie=UTF8&amp;hq=gimnazjum+nr+3&amp;hnear=Bydgoszcz,+Kujawsko-Pomorskie,+Polska&amp;cid=2821239990550035673&amp;ll=53.178469,18.174133&amp;spn=0.072017,0.164795&amp;z=12&amp;iwloc=A&amp;output=embed"></iframe> -->
+					
+
+						<div class="extra-wrap p8"><br> <br> <br>
+
+
+
+							<h3>Uwaga::</h3>
+
+							<p>
+
+								<b>W poniedziałek 21.03.2016 biuro bedzie nieczynne.<br>
+									<br>
+
+									Za utrudnienia przepraszamy<br> <br> <br>
+
+								</b>
+
+							</p>
+							-->
+
+
+
+							<h2 class="padd-1">
+
+								Godziny pracy biura:</h2>
+							<h3></h3>
+							<p>
+
+								<b>Poniedziałek: 18:20-19:20 <br> Wtorek: 17:00-18:00<br>
+								 Środa: nieczynne <br>
+								 Czwartek: nieczynne <br>
+								 Piątek: nieczynne  <br>
+								 
+									</b>
+
+							</p>
+						</div>
+					
+
+
+
+				</div>
+				<div class="grid_5 marg-right-0">
+					<em><img src="images/logo.png" alt="Angielski Bydgoszcz"
+						style="width: 110px; height: 40px;"></em>
+					<dl class="adress">
+
+						<dt class="title1 fot6">
+							<b>Biuro:</b><br>ul. Gawędy 5 (Zespół Szkół nr 35)<br>85-792
+							Bydgoszcz (os. Bajka)
+						</dt>
+
+
+						<dd>
+							<br> <span>Telefon:</span>
+							<span2 style="font-size:20px;font-weight:bold;">+48 782 213 664</span2>
+						</dd>
+
+						<dd>
+							<span><br>E-mail:</span> <a href="#"><br>biuro@smileschool.pl</a>
+						</dd>
+						</br>
+						</br>
+						<dt class="title1">
+							<b>Siedziba firmy (adres korespondencyjny):</b><br>ul. G.O.P.R.
+							3/1<br> 85-794 Bydgoszcz
+						</dt>
+
+						<dd>
+							<span>NIP:</span>5542585695
+						</dd>
+						<dd>
+							<span>REGON:</span>341335653
+						</dd>
+						<dd>
+							<span>Nr konta:</span>61 1140 2004 0000 3802 7443 4474
+						</dd>
+					</dl>
+					<dl class="adress">
+
+					</dl>
+
+				</div>
+			</div>
+		</div>
+		<div class="clear"></div>
+	</div>
+</section>
